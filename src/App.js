d4d061b5cdd9a25c44f77288a708027b6a27c9dc
Joe Vinten <joseph.vinten@gmail.com>
2021-01-14 22:35:32 +0000

    build: made API request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, LineSeries, VerticalGridLines, XAxis, YAxis} from 'react-vis';
 
 function App() {
+
+  let data = []
+    const getVaccineData = async() => {
+      const response = await fetch(
+        "https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=overview&structure={%22date%22:%22date%22,%22cumPeopleVaccinatedFirstDoseByPublishDate%22:%22cumPeopleVaccinatedFirstDoseByPublishDate%22}",
+        {
+          mode: 'cors',
+        }
+      )
+    const responseData = await response.json();
+    const vaccineData = responseData.data;
+    console.log(data)
+
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
     </div>
-  );
+    );
 }
 
 export default App;
